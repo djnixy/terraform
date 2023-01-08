6577b0e7d954d8dc26593e2126385f5bb5b0cb7e
Niki Akbar <nikiakbar@gmail.com>
2023-01-08 17:14:25 +0700

    Update

--- aws-ecs/provider.tf
+++ aws-ecs/provider.tf
@@ -1,12 +1,9 @@
-# provider "aws" {
-#     region = "ap-southeast-2"
-#     profile = "gs"
-# }
 
 provider "aws" {
     region = "ap-southeast-1"
-    access_key = var.aws_access_key_id
-    secret_key = var.aws_secret_access_key
+    profile                  = "default"
+    # access_key = var.aws_access_key_id
+    # secret_key = var.aws_secret_access_key
 
     default_tags {
         tags = {
