0a0ce87c29dcdc541e50bb5df8feb274ed1aa8c9
Niki Akbar <nikiakbar@gmail.com>
2024-07-25 20:26:06 +0700

    update 6 files and delete 1 file

--- AWS/generate_data.tm.hcl
+++ AWS/generate_data.tm.hcl
@@ -13,6 +13,34 @@ data "aws_region" "current" {}
 data "aws_availability_zones" "available" {}
 data "aws_ami" "eks_default" {}
 data "aws_ami" "eks_default_bottlerocket" {}
+## VPC
+data "aws_vpc" "default" {
+  default = true
+}
 
+data "aws_subnet" "zone-a" {
+  vpc_id = data.aws_vpc.default.id
+  filter {
+    name   = "availability-zone"
+    values = ["${var.deploy_region}a"]
+  }
+}
+
+data "aws_subnet" "zone-b" {
+  vpc_id = data.aws_vpc.default.id
+  filter {
+    name   = "availability-zone"
+    values = ["${var.deploy_region}b"]
+  }
+}
+
+output "subnet_id_az_a" {
+  value = data.aws_subnet.zone-a.id
+}
+
+output "subnet_id_az_b" {
+  value = data.aws_subnet.zone-b.id
+}
+    
   }
 }
--- AZURE/stacks/az-appservice-firewall/variables.tf
+++ AZURE/stacks/az-appservice-firewall/variables.tf
@@ -10,59 +10,47 @@ variable "serviceName" {
   default = ""
 }
 
-variable "environment" {
-  # description = ""
-  type        = string
-  default = "development"
-}
+# variable "sqlAdminName" {
+#   # description = ""
+#   type        = string
+#   default = "sqladmin"
+# }
 
-variable "region" {
-  # description = ""
-  type        = string
-  default = ""
-}
+# variable "sqlAdminPassword" {
+#   # description = ""
+#   type        = string
+#   default = "NOTsoSecurePassword893!"
+# }
 
-variable "sqlAdminName" {
-  # description = ""
-  type        = string
-  default = "sqladmin"
-}
+# variable "databaseName" {
+#   # description = ""
+#   type        = string
+#   default = "api"
+# }
 
-variable "sqlAdminPassword" {
-  # description = ""
-  type        = string
-  default = "NOTsoSecurePassword893!"
-}
+# locals {
+#   # Common tags to be assigned to all resources
+#   # common_tags = {
+#   #   Service = local.service_name
+#   #   Owner   = local.owner
+#   # }
+#     http_setting_name =  join("-", ["behtst", var.productName, var.environment])
+#     listener_name = join("-", ["httplstn", var.productName, var.environment])
+#     request_routing_rule_name = join("-", ["rqrt", var.productName, var.environment])
+#     frontend_port_name = join("-", ["feport", var.productName, var.environment])
+#     frontend_ip_configuration_name = join("-", ["feip", var.productName, var.environment])
+#     backend_address_pool_name = join("-", ["beap", var.productName, var.environment])
+#     public_ip_address_name = join("-", ["pip", var.productName, var.environment])
+#     rgName          = join("-", ["rg", var.productName, var.environment])
+#     waf_policy_name = join("-", ["waf", var.productName, var.environment])
+#     appGatewayName  = join("-", ["agw", var.productName, var.environment])
+#     vnetName        = join("-", ["vnet", var.productName, var.environment])
+#     planName        = join("-", ["plan", var.productName, var.environment])
+#     appName         = join("-", ["app", var.productName, var.serviceName, var.environment])
 
-variable "databaseName" {
-  # description = ""
-  type        = string
-  default = "api"
-}
-
-locals {
-  # Common tags to be assigned to all resources
-  # common_tags = {
-  #   Service = local.service_name
-  #   Owner   = local.owner
-  # }
-    http_setting_name =  join("-", ["behtst", var.productName, var.environment])
-    listener_name = join("-", ["httplstn", var.productName, var.environment])
-    request_routing_rule_name = join("-", ["rqrt", var.productName, var.environment])
-    frontend_port_name = join("-", ["feport", var.productName, var.environment])
-    frontend_ip_configuration_name = join("-", ["feip", var.productName, var.environment])
-    backend_address_pool_name = join("-", ["beap", var.productName, var.environment])
-    public_ip_address_name = join("-", ["pip", var.productName, var.environment])
-    rgName          = join("-", ["rg", var.productName, var.environment])
-    waf_policy_name = join("-", ["waf", var.productName, var.environment])
-    appGatewayName  = join("-", ["agw", var.productName, var.environment])
-    vnetName        = join("-", ["vnet", var.productName, var.environment])
-    planName        = join("-", ["plan", var.productName, var.environment])
-    appName         = join("-", ["app", var.productName, var.serviceName, var.environment])
-
-    sqlServerName   = join("-", ["mysql", var.productName, var.environment]) 
-    # sqlPoolName     = join("-", ["pool", var.productName, var.environment])
-    # sqlDBName       = join("-", ["sqldb", var.databaseName , var.environment])
-    # sqlDBName1      = join("-", ["sqldb", var.databaseName1 , var.environment])
-}
+#     sqlServerName   = join("-", ["mysql", var.productName, var.environment]) 
+#     # sqlPoolName     = join("-", ["pool", var.productName, var.environment])
+#     # sqlDBName       = join("-", ["sqldb", var.databaseName , var.environment])
+#     # sqlDBName1      = join("-", ["sqldb", var.databaseName1 , var.environment])
+# }
 
